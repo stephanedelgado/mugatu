38128dc47bb9487f269991917ee77e2da857b69a
Allen Sanchez <allenjsanchez@gmail.com>
2017-03-21 18:16:03 +0800

    refactored carousel'

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170321042054) do
-
+ActiveRecord::Schema.define(version: 20170321043253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +31,6 @@ ActiveRecord::Schema.define(version: 20170321042054) do
     t.index ["user_id"], name: "index_artists_on_user_id", using: :btree
   end
 
-<<<<<<< HEAD
-=======
   create_table "attachinary_files", force: :cascade do |t|
     t.string   "attachinariable_type"
     t.integer  "attachinariable_id"
@@ -50,7 +46,6 @@ ActiveRecord::Schema.define(version: 20170321042054) do
     t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent", using: :btree
   end
 
->>>>>>> master
   create_table "bookings", force: :cascade do |t|
     t.string   "address"
     t.datetime "start_time"
