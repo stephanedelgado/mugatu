8508a8ea36e2fefd7431bd246516271e285f85e9
Allen Sanchez <allenjsanchez@gmail.com>
2017-03-22 11:56:38 +0800

    added geocoder gem

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -13,4 +13,6 @@ class Artist < ApplicationRecord
   validates :phone_number, presence: true
   validates_format_of :phone_number, with: /^(\+86)(13[0-9]|145|147|15[0-3,5-9]|18[0,2,5-9])(\d{8})$/,  :multiline => true
   validates :gender, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
