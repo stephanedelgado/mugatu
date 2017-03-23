d72f50009f73e9394d61d5bc90d2cdcb5c22668f
John Roberts <john.calvin.roberts@gmail.com>
2017-03-23 13:13:04 +0800

    added city to artists. also added a note on sign up form to add a minimum of 3 photos.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322033445) do
+ActiveRecord::Schema.define(version: 20170323041550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170322033445) do
     t.string   "gender"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "city"
     t.index ["user_id"], name: "index_artists_on_user_id", using: :btree
   end
 
