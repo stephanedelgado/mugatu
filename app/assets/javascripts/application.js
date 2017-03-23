1b79e522c301f1e66467b51f5cc4d80560cca205
Stephane Delgado <stephanedelgado@gmail.com>
2017-03-23 19:11:31 +0800

    new dash

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,15 @@
 
 $('.input-daterange').datepicker({
 });
+
+$(function(){
+  $(".tab").on("click", function(e){
+    // Change active tab
+    $(".tab").removeClass('active');
+    $(this).addClass('active');
+    // Hide all tab-content (use class="hidden")
+    $(".tab-content").addClass('hidden');
+    // Show target tab-content (use class="hidden")
+    $($(this).data('target')).removeClass('hidden');
+  });
+});
