b7efcb8404216fa99307c65fa27fe1cac0a57cef
John Roberts <john.calvin.roberts@gmail.com>
2017-03-23 19:10:38 +0800

    Sticky footer

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :artist
   belongs_to :user
-  validates :address, presence: true
+ # validates :address
   validates :start_time, presence: true
   validates :end_time, presence: true
   validates :status, inclusion: { in: [ "pending", "accepted", "rejected"], allow_nil: false }
