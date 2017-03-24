65a14b8b58092de867d29d6490dc72de2886f183
Stephane Delgado <stephanedelgado@gmail.com>
2017-03-24 12:35:14 +0800

    cloudinary settings for avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
-  def avatar_tag
+  def avatar_tag(width = 40, height = 40)
     if current_user.facebook_picture_url
       image_tag current_user.facebook_picture_url, class: "avatar dropdown-toggle", id: "navbar-wagon-menu", "data-toggle" => "dropdown"
     elsif current_user.avatar?
-      cl_image_tag current_user.avatar.path, class: "avatar dropdown-toggle", id: "navbar-wagon-menu", "data-toggle" => "dropdown"
+      cl_image_tag current_user.avatar.path, height: height, width: width, crop: :fill, gravity: :face, class: "avatar dropdown-toggle", id: "navbar-wagon-menu", "data-toggle" => "dropdown"
     else
       image_tag "http://placehold.it/30x30", class: "avatar dropdown-toggle", id: "navbar-wagon-menu", "data-toggle" => "dropdown"
     end
   end
 end
+
